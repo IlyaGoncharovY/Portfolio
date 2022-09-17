220988b3c0dd1a414bdc5f3235a50d7017c3053d
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-09-17 17:01:52 +0300

    refactor style add form contact

--- src/Footer/MyFooter.tsx
+++ src/Footer/MyFooter.tsx
@@ -5,9 +5,43 @@ import {Title} from "../common/components/title/Title";
 import telegram from "../assects/image/telegram111.png"
 import github from "../assects/image/github.png"
 import linkedin from "../assects/image/linkedin1.png"
+import {useFormik} from "formik";
+
+type ValuesType = {
+    name?: string;
+    email?: string;
+    message?: string;
+}
 
 
 export const MyFooter = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            message: '',
+
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+        validate: (values) => {
+            const errors: ValuesType = {}
+
+            if (!values.email) {
+                errors.email = 'required'
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+
+            if (values.message.length < 2) {
+                errors.message = 'message must be more two symbols'
+            }
+
+            return errors
+        },
+    });
 
     const telegram1 = {
         backgroundImage: 'url(' + telegram + ')',
@@ -24,7 +58,44 @@ export const MyFooter = () => {
     return (
         <div className={s.footerBlock}>
             <div className={`${styleContainer.container} ${s.footerContainer}`}>
-                <Title title={"Ilya Goncharov"} titleBg={"@Contact`s"}/>
+                <Title title={"Contact"} titleBg={"Contact"}/>
+                    <form onSubmit={formik.handleSubmit} className={s.formContainer}>
+                        <label htmlFor="Name" className={s.label}>Name</label>
+                        <input
+                            id="name"
+                            name="name"
+                            type="text"
+                            onChange={formik.handleChange}
+                            value={formik.values.name}
+                            className={s.inputForm}
+                        />
+
+                        <label htmlFor="Email" className={s.label}>Email</label>
+                        <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            onChange={formik.handleChange}
+                            value={formik.values.email}
+                            className={s.inputForm}
+                        />
+                        {formik.touched.email && formik.errors.email &&
+                            <div style={{color: 'red'}}>{formik.errors.email}</div>}
+
+                        <label htmlFor="Message" className={s.label}>Message</label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            onChange={formik.handleChange}
+                            value={formik.values.message}
+                            className={s.inputForm}
+                        />
+                        {formik.touched.message && formik.errors.message &&
+                            <div style={{color: 'red'}}>{formik.errors.message}</div>}
+
+                        <button type="submit" className={s.buttonForm}>Submit</button>
+                    </form>
+
                 <div className={s.boxContainer}>
                     <a href={"https://t.me/ilyaGoncharov93"}>
                         <div className={s.box} style={telegram1}>
--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -7,12 +7,12 @@ export const Navigation = () => {
     return (
         <div className={s.nav}>
             <ul className={s.topNavigation}>
-                <li className={s.I}><NavLink to={"/Portfolio"}>{<Icon icon="line-md:home" width={"30"} color={"#212529"}/>}</NavLink></li>
-                <li className={s.I}><NavLink to={"/about_me"}>{<Icon icon="line-md:account" width={"30"} color={"#212529"}/>}</NavLink></li>
-                <li className={s.I}><NavLink to={"/skills"}>{<Icon icon="line-md:laptop" width={"30"} color={"#212529"}/>}</NavLink></li>
-                <li className={s.I}><NavLink to={"/projects"}>{<Icon icon="line-md:document-code" width={"30"} color={"#212529"}/>}</NavLink></li>
+                <li className={s.I}><NavLink to={"/Portfolio"}>{<Icon icon="line-md:home" width={"24"} color={"#212529"}/>}</NavLink></li>
+                <li className={s.I}><NavLink to={"/about_me"}>{<Icon icon="line-md:account" width={"24"} color={"#212529"}/>}</NavLink></li>
+                <li className={s.I}><NavLink to={"/skills"}>{<Icon icon="line-md:laptop" width={"24"} color={"#212529"}/>}</NavLink></li>
+                <li className={s.I}><NavLink to={"/projects"}>{<Icon icon="line-md:document-code" width={"24"} color={"#212529"}/>}</NavLink></li>
                 {/*<li className={s.I}><NavLink to={"/contacts"}>{<Icon icon="line-md:email-opened" width={"30"} color={"#212529"}/>}</NavLink></li>*/}
-                <li className={s.I}><NavLink to={"/footer"}>{<Icon icon="line-md:email-opened" width={"30"} color={"#212529"}/>}</NavLink></li>
+                <li className={s.I}><NavLink to={"/footer"}>{<Icon icon="line-md:email-opened" width={"24"} color={"#212529"}/>}</NavLink></li>
             </ul>
         </div>
 

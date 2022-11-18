c2258b236439c4580625e8565c0eb02ef769425c
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-11-19 01:31:37 +0300

    fix(style) hover for Navigation.tsx

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -7,12 +7,77 @@ export const Navigation = () => {
     return (
         <div className={s.nav}>
             <ul className={s.topNavigation}>
-                <li className={s.I}><NavLink to={"/Portfolio"}>{<Icon icon="line-md:home" width={"24"} color={"#212529"}/>}</NavLink></li>
-                <li className={s.I}><NavLink to={"/about_me"}>{<Icon icon="line-md:account" width={"24"} color={"#212529"}/>}</NavLink></li>
-                <li className={s.I}><NavLink to={"/skills"}>{<Icon icon="line-md:laptop" width={"24"} color={"#212529"}/>}</NavLink></li>
-                <li className={s.I}><NavLink to={"/projects"}>{<Icon icon="line-md:document-code" width={"24"} color={"#212529"}/>}</NavLink></li>
-                {/*<li className={s.I}><NavLink to={"/contacts"}>{<Icon icon="line-md:email-opened" width={"30"} color={"#212529"}/>}</NavLink></li>*/}
-                <li className={s.I}><NavLink to={"/footer"}>{<Icon icon="line-md:email-opened" width={"24"} color={"#212529"}/>}</NavLink></li>
+                <li className={s.I}>
+                    <div className={s.bodyIcon}>
+                        <NavLink to={"/Portfolio"} className={s.linkA}>
+                            {<Icon icon="line-md:home" width={"24"} color={"#212529"} className={s.icon}/>}
+                            <div className={s.bodyIconChild}>
+                                <span className={s.spanEl}>
+                                    Home
+                                </span>
+                            </div>
+                        </NavLink>
+                    </div>
+                </li>
+                <li className={s.I}>
+                    <div className={s.bodyIcon}>
+                        <NavLink to={"/about_me"} className={s.linkA}>
+                            {<Icon icon="line-md:account" width={"24"} color={"#212529"}/>}
+                            <div className={s.bodyIconChild}>
+                                <span className={s.spanEl}>
+                                    <div>
+                                       About
+                                    </div>
+                                    <div>
+                                        Me
+                                    </div>
+                               </span>
+                            </div>
+                        </NavLink>
+                    </div>
+                </li>
+                <li className={s.I}>
+                    <div className={s.bodyIcon}>
+                        <NavLink to={"/skills"} className={s.linkA}>
+                            {<Icon icon="line-md:laptop" width={"24"} color={"#212529"}/>}
+                            <div className={s.bodyIconChild}>
+                                <span className={s.spanEl}>
+                                    Skills
+                                </span>
+                            </div>
+                        </NavLink>
+                    </div>
+
+                </li>
+                <li className={s.I}>
+                    <div className={s.bodyIcon}>
+                        <NavLink to={"/projects"} className={s.linkA}>
+                            {<Icon icon="line-md:document-code" width={"24"} color={"#212529"}/>}
+                            <div className={s.bodyIconChild}>
+                                <span className={s.spanEl}>
+                                    Project
+                                </span>
+                            </div>
+                        </NavLink>
+                    </div>
+                </li>
+                <li className={s.I}>
+                    <div className={s.bodyIcon}>
+                        <NavLink to={"/footer"} className={s.linkA}>
+                            {<Icon icon="line-md:email-opened" width={"24"} color={"#212529"}/>}
+                            <div className={s.bodyIconChild}>
+                                <span className={s.spanEl}>
+                                    <div>
+                                        Call
+                                    </div>
+                                    <div>
+                                         Me
+                                    </div>
+                                </span>
+                            </div>
+                        </NavLink>
+                    </div>
+                </li>
             </ul>
         </div>
 

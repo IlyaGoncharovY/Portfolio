9573db8ac057debf656e6cdf88a8c87961a19f88
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-01-14 00:17:50 +0300

    fix(download) added the ability to download cv from the wanderer

--- src/mainBlock/MainBlock.tsx
+++ src/mainBlock/MainBlock.tsx
@@ -3,12 +3,23 @@ import s from "./Main.module.scss"
 import styleContainer from "../common/styles/Contauner.module.css"
 import ilya from "../assects/image/ilya.jpg";
 import Tilt from 'react-parallax-tilt';
+// @ts-ignore
+import cv from "../assects/CV/CvIlya.pdf"
 
 export const MainBlock = () => {
 
     const ilya1 = {
         backgroundImage: 'url(' + ilya + ')',
     };
+
+    const downloadFile = () => {
+        const file = document.createElement("a")
+        file.href = cv
+        file.setAttribute("download", "CvIlya.pdf")
+        document.body.append(file)
+        file.click()
+        file.remove()
+    }
 
     return (
         <div className={s.mainBlock}>
@@ -21,12 +32,14 @@ export const MainBlock = () => {
                     <div className={s.textEl}>
                         <h1>- I am Ilya Goncharov.</h1>
                         <h1 className={s.mainTitle}>Frontend Developer.</h1>
-                        <p className={s.aboutText}>   Hello friend!
+                        <p className={s.aboutText}> Hello friend!
                             I am a novice front‑end developer specializing
                             in creating a user interface.
                             I am passionate about creating great software
                             that improves the lives of the people around me. </p>
+                        <button onClick={downloadFile} className={s.button}><span>Check my CV</span></button>
                     </div>
+
                 </div>
             </div>
         </div>

12df3cc95e9ca0e0e4dd644a97264229a7989198
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-02-21 15:38:20 +0300

    fix(cv, chat) change cv file, added chat project

--- src/projects/MyProject.tsx
+++ src/projects/MyProject.tsx
@@ -6,6 +6,7 @@ import {Project} from "./project/Project";
 import socialImage from "../assects/image/social1.png"
 import cardImg from "../assects/image/cardImg.png"
 import todoImg from "../assects/image/todoImg.png"
+import chat from "../assects/image/chat.png"
 
 export const MyProjects = () => {
 
@@ -21,23 +22,30 @@ export const MyProjects = () => {
         backgroundImage: 'url(' + todoImg + ')'
     }
 
+    const chatImg = {
+        backgroundImage: `url(` + chat + `)`
+    }
+
     return (
         <div className={s.myWorkBlock}>
             <div className={`${styleContainer.container} ${s.myWorkContainer}`}>
                 <Title title={"Projects"} titleBg={"Works"}/>
                 <div className={s.works}>
-                        <Project url={`https://ilyagoncharovy.github.io/todolist-ts/`}
-                                 style={todolistImg} title={"todolist"}
-                                 description={"ability to create and edit tasks"}/>
-                        <Project url={`https://ilyagoncharovy.github.io/fridays-project/`}
-                                 style={cardImage} title={"Learn card"}
-                                 description={"working with maps for training, in any field"}/>
-                        <Project url={"https://ilyagoncharovy.github.io/SocialNetworkHomeWork/"}
-                                 style={social} title={"social network"}
-                                 description={"social network, training project"}/>
+                    <Project url={`https://ilyagoncharovy.github.io/todolist-ts/`}
+                             style={todolistImg} title={"todolist"}
+                             description={"ability to create and edit tasks"}/>
+                    <Project url={`https://ilyagoncharovy.github.io/fridays-project/`}
+                             style={cardImage} title={"Learn card"}
+                             description={"working with maps for training, in any field"}/>
+                    <Project url={"https://ilyagoncharovy.github.io/SocialNetworkHomeWork/"}
+                             style={social} title={"social network"}
+                             description={"social network, training project"}/>
+                    <Project url={"https://github.com/IlyaGoncharovY/my-chat"}
+                             style={chatImg} title={"chat"}
+                             description={"chat on websockets"}/>
                 </div>
             </div>
         </div>
-);
+    );
 };
 

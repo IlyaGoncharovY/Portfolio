dbc2186ae62968070efbdc02c0d85f8fa822cbd7
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-03-04 02:32:57 +0300

    fix(Social) added dataSet, refactor style MyFooter.module.scss, social.module.scss.

---
+++ src/common/data/urlImg.tsx
@@ -0,0 +1,35 @@
+import todoImg from "../../assects/image/todoImg.png";
+import cardImg from "../../assects/image/cardImg.png";
+import socialImage from "../../assects/image/social1.png";
+import chat from "../../assects/image/chat.png";
+import telegram from "../../assects/image/telegram111.png";
+import github from "../../assects/image/github.png";
+import linkedin from "../../assects/image/linkedin1.png";
+
+export const todolistImg = {
+    backgroundImage: 'url(' + todoImg + ')'
+}
+
+export const cardImage = {
+    backgroundImage: 'url(' + cardImg + ')',
+};
+
+export const social = {
+    backgroundImage: 'url(' + socialImage + ')',
+};
+
+export const chatImg = {
+    backgroundImage: `url(` + chat + `)`
+}
+
+export const telegram1 = {
+    backgroundImage: 'url(' + telegram + ')',
+}
+
+export const github1 = {
+    backgroundImage: 'url(' + github + ')',
+}
+
+export const linkedin1 = {
+    backgroundImage: 'url(' + linkedin + ')',
+}
